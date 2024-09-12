2c79a100349d129b03d9a4ed80953729258d5b2f
locky <onerd170@gmail.com>
2024-09-12 14:55:34 -0300

    adiniona inquirer

--- index.js
+++ index.js
@@ -1,47 +1,17 @@
-// // hello world //
-
-// const mensagem = "ola,mundo!"
-// {
-//     const mensagem ="Ola eu"
-//     console.log(mensagem);
-// }
-// let numero = 2 
-// console.log(mensagem);
-// console.log(numero);
-
-//---------------------------------
-// arrays, objetos
-
-//let metas = ["luan", "Opa"]
-//console.log(metas[1] +", " + metas [0])
-
-let meta = {
-    value: "ler um livro por mês",
-    checked: true,
-}  
-let metas =[
-    meta,
-    {
-        value:"correr 20 minutes",
-        checked:false
+const start = () => {
+    let count = 1
+    while (true) {
+        let opcao = "cadastrar"
+        switch (opcao) {
+            case "cadastrar":
+                console.log ("Vamos cadastrar")
+                break              
+                case "listar":
+                    console.log("vamos listar")
+                break;
+            case "sair": 
+                return
+        }
     }
-]
-
-//meta.value = "ler um livro por mês"
-//meta.log(meta.value)
-
-console.log(metas[1].value)
-
-
-
-
-//---------------------------------
-// function // arrow function
-
-//const criarMeta = () => {} 
-
-//function criarMeta(params) {
-    
-//}
-
-
+}
+start()
